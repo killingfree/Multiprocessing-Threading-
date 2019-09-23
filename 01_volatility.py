2c3e3d0ad3460ce92597161979b64179fa6e3dd4
gr1g@mail.ru <byltynbabrfnjh222>
2019-09-23 10:41:59 +0300

    the task is to find the volatility of the Ticker.Description is in volatility.py

---
+++ 01_volatility.py
@@ -0,0 +1,133 @@
+# -*- coding: utf-8 -*-
+
+
+# Описание предметной области:
+#
+# При торгах на бирже совершаются сделки - один купил, второй продал.
+# Покупают и продают ценные бумаги (акции, облигации, фьючерсы, етс). Ценные бумаги - это по сути долговые расписки.
+# Ценные бумаги выпускаются партиями, от десятка до несколько миллионов штук.
+# Каждая такая партия (выпуск) имеет свой торговый код на бирже - тикер - https://goo.gl/MJQ5Lq
+# Все бумаги из этой партии (выпуска) одинаковы в цене, поэтому говорят о цене одной бумаги.
+# У разных выпусков бумаг - разные цены, которые могут отличаться в сотни и тысячи раз.
+# Каждая биржевая сделка характеризуется:
+#   тикер ценнной бумаги
+#   время сделки
+#   цена сделки
+#   обьем сделки (сколько ценных бумаг было куплено)
+#
+# В ходе торгов цены сделок могут со временем расти и понижаться. Величина изменения цен называтея волатильностью.
+# Например, если бумага №1 торговалась с ценами 11, 11, 12, 11, 12, 11, 11, 11 - то она мало волатильна.
+# А если у бумаги №2 цены сделок были: 20, 15, 23, 56, 100, 50, 3, 10 - то такая бумага имеет большую волатильность.
+# Волатильность можно считать разными способами, мы будем считать сильно упрощенным способом -
+# отклонение в процентах от средней цены за торговую сессию:
+#   средняя цена = (максимальная цена + минимальная цена) / 2
+#   волатильность = ((максимальная цена - минимальная цена) / средняя цена) * 100%
+# Например для бумаги №1:
+#   average_price = (12 + 11) / 2 = 11.5
+#   volatility = ((12 - 11) / average_price) * 100 = 8.7%
+# Для бумаги №2:
+#   average_price = (100 + 3) / 2 = 51.5
+#   volatility = ((100 - 3) / average_price) * 100 = 188.34%
+#
+# В реальности волатильность рассчитывается так: https://goo.gl/VJNmmY
+#
+# Задача: вычислить 3 тикера с максимальной и 3 тикера с минимальной волатильностью.
+# Бумаги с нулевой волатильностью вывести отдельно.
+# Результаты вывести на консоль в виде:
+#   Максимальная волатильность:
+#       ТИКЕР1 - ХХХ.ХХ %
+#       ТИКЕР2 - ХХХ.ХХ %
+#       ТИКЕР3 - ХХХ.ХХ %
+#   Минимальная волатильность:
+#       ТИКЕР4 - ХХХ.ХХ %
+#       ТИКЕР5 - ХХХ.ХХ %
+#       ТИКЕР6 - ХХХ.ХХ %
+#   Нулевая волатильность:
+#       ТИКЕР7, ТИКЕР8, ТИКЕР9, ТИКЕР10, ТИКЕР11, ТИКЕР12
+# Волатильности указывать в порядке убывания. Тикеры с нулевой волатильностью упорядочить по имени.
+#
+# Подготовка исходных данных
+# 1. Скачать файл https://drive.google.com/file/d/1l5sia-9c-t91iIPiGyBc1s9mQ8RgTNqb/view?usp=sharing
+#       (обратите внимание на значок скачивания в правом верхнем углу,
+#       см https://drive.google.com/file/d/1M6mW1jI2RdZhdSCEmlbFi5eoAXOR3u6G/view?usp=sharing)
+# 2. Раззиповать средствами операционной системы содержимое архива
+#       в папку python_base_source/lesson_012/trades
+# 3. В каждом файле в папке trades содержится данные по сделакам по одному тикеру, разделенные запятыми.
+#   Первая строка - название колонок:
+#       SECID - тикер
+#       TRADETIME - время сделки
+#       PRICE - цена сделки
+#       QUANTITY - количество бумаг в этой сделке
+#   Все последующие строки в файле - данные о сделках
+#
+# Подсказка: нужно последовательно открывать каждый файл, вычитывать данные, высчитывать волатильность и запоминать.
+# Вывод на консоль можно сделать только после обработки всех файлов.
+
+
+import os
+
+
+
+class FileTreatment:
+    def __init__(self, path):
+        self.path = path
+        self.volatility = None
+        self.ticker = None
+
+    def run(self):
+        with open(self.path) as read_file:
+            self.unpacking_file(read_file)
+        return self.volatility, self.ticker
+
+    def unpacking_file(self, read_file):
+        minimum_value = None
+        maximum_value = None
+        is_first_line = True
+        for _, line in enumerate(read_file):
+            if is_first_line:
+                is_first_line = False
+            else:
+                self.ticker, trade_time, price, quantity = line.split(",")
+                check_price = float(price)
+                if minimum_value is None:
+                    minimum_value = check_price
+                    maximum_value = check_price
+                elif minimum_value > check_price:
+                    minimum_value = check_price
+                elif maximum_value < check_price:
+                    maximum_value = check_price
+        self.calculations_file_and_sorted(maximum_value, minimum_value)
+
+    def calculations_file_and_sorted(self, maximum_value, minimum_value):
+        if maximum_value is None or minimum_value is None:
+            raise ValueError("ошибка расчета формулы. Одно из значений равно нулю")
+        else:
+            average_price = (maximum_value + minimum_value) / 2
+            self.volatility = round(((maximum_value - minimum_value) / average_price) * 100, 2)
+
+
+zero_volatility = []
+all_volatility = []
+for dirpath, _, filenames in os.walk("trades"):
+    for file in filenames:
+        path = os.path.join(dirpath, file)
+        trade = FileTreatment(path)
+        volatility, ticker = trade.run()
+        if volatility == 0:
+            zero_volatility.append(ticker)
+        else:
+            all_volatility.append((volatility, ticker))
+
+all_volatility.sort(reverse=True)
+zero_volatility.sort()
+max_volatility = all_volatility[0:3]
+min_volatility = all_volatility[-3::]
+print('максимальная волотилитильность:')
+for element in max_volatility:
+    print(element)
+print('минимальная волотилитильность:')
+for element in min_volatility:
+    print(element)
+print("нулевая волотильность:")
+print(zero_volatility)
+
